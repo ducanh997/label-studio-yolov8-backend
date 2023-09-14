dda84807c98a5f14e500094e5ddde8286da06e70
Nguyen Duc Anh <ducanhk60uet@gmail.com>
2023-09-14 16:27:14 +0700

    Update code

--- _wsgi.py
+++ _wsgi.py
@@ -35,3 +35,6 @@ app = init_app(
     redis_host=os.environ.get('REDIS_HOST', 'localhost'),
     redis_port=os.environ.get('REDIS_PORT', 6379)
 )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8002, debug=True, load_dotenv=True)
